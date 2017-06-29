91d8815bbf7111b5163b46ca2cdbefc8640a0c05
jawedm <jawedm@cakesolutions.net>
2017-06-29 14:26:27 +0100

    relative path

--- src/main/java/bankingObjects/TestBase.java
+++ src/main/java/bankingObjects/TestBase.java
@@ -46,8 +46,7 @@ public class TestBase {
           caps.setCapability(
                     PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
                     //"/Users/jawedmokhtar/.jenkins/jobs/docker/workspace/binary/phantomjs_binary"
-                  "/home/jenkins/.jenkins/workspace/manual-pipeline/IntegrationTests/node_modules/phantomjs-prebuilt"
-//                  "/home/jenkins/.jenkins/workspace/manual-pipeline/IntegrationTests/node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs"
+                  "IntegrationTests/node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs"
             );
 
             driver= new PhantomJSDriver(caps);
